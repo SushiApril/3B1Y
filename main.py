c9f6e55da39883fbfc88e173187da0d1b3153ee4
Sushant Gupta <97147760+SushiApril@users.noreply.github.com>
2024-10-19 12:57:34 -0700

    Update main.py move left doggo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     #example of using micro and speakers
     print("Start recording audio")
     sample_name = "aaaa.wav"
-    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
+    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=5 -c 1 {sample_name}'
     print(cmd)
     os.system(cmd)
     print("Playing sound")
@@ -42,12 +42,12 @@ def main():
         time.sleep(3)
 
         # Make Spot to move by goal_x meters forward and goal_y meters left
-        spot.move_to_goal(goal_x=0.5, goal_y=0)
+        spot.move_to_goal(goal_x=1.0, goal_y=0.5)
         time.sleep(3)
         capture_image()
 
         # Control Spot by velocity in m/s (or in rad/s for rotation)
-        spot.move_by_velocity_control(v_x=-0.3, v_y=0, v_rot=0, cmd_duration=2)
+        spot.move_by_velocity_control(v_x=-0.5, v_y=-0.3, v_rot=0, cmd_duration=3)
         capture_image()
         time.sleep(3)
 
