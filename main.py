5f7fecc612acc23f16bbf2aa7ab7d5c3ef70ac18
Sushant Gupta <97147760+SushiApril@users.noreply.github.com>
2024-10-19 17:22:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -71,17 +71,19 @@ def move_forward(spot):
 def move_backward(spot):
     spot.move_to_goal(goal_x=-0.5, goal_y=0.0)
 
+def turn_left(spot):
+    spot.move_by_velocity_control(v_x=-0.0, v_y=-1.0, v_rot=0, cmd_duration=3)
+
+def turn_right(spot):
+    spot.move_by_velocity_control(v_x=-1.0, v_y=-0.0, v_rot=0, cmd_duration=3)
+
 def strafe_left(spot):
     velocity_cmd_helper(spot, v_y=0.5)
 
 def strafe_right(spot):
     velocity_cmd_helper(spot, v_y=-0.5)
 
-def turn_left(spot):
-    velocity_cmd_helper(spot, v_rot=0.8)
 
-def turn_right(spot):
-    velocity_cmd_helper(spot, v_rot=-0.8)
 
 def run():
 
